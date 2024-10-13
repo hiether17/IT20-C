76e2f0a7616d2fe6753a6a5c431499c19bcc0621
hiether17 <lhietherfhiona@gmail.com>
2024-10-13 11:38:09 +0800

    Added case1 that display size

--- src/Topic_3_stack/main.java
+++ src/Topic_3_stack/main.java
@@ -12,5 +12,20 @@ public class main {
     	
     	System.out.println("Hello Love");
     	
+    	  while(true) {
+              System.out.println("Choose what you want to do: "
+                       + " 1. Size"
+                       + " 2. Push"
+                       + " 3. Pop"
+                      +  " 4. Exit");
+               int choice = sc.nextInt();
+           switch(choice) {
+           
+           case 1:
+               System.out.println("Stack size is: " + stack.size());
+               System.out.println("~~~~~~~~~~~~~~~~~~");
+               break;
+    }
+}
     }
 }
