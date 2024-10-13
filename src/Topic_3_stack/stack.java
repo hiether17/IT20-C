ca00881cf73cfd3dac5227d5918c009c04bc20ff
hiether17 <lhietherfhiona@gmail.com>
2024-10-13 12:08:52 +0800

    Added some changes

--- src/Topic_3_stack/stack.java
+++ src/Topic_3_stack/stack.java
@@ -4,4 +4,11 @@ public class stack {
 	private int maxSize;
     private int[] stackArray;
     private int top; 
+    
+    public stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+        
+}
 }
